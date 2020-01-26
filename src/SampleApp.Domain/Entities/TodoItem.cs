eca79c9a92b64d5d76f414c1e989690df00116be
Mahdi Ghorbanpoor <mahdi.ghorbanpoor@pgk.de>
2020-01-26 13:25:03 +0100

    Added entities based on CleanArchitecture sample

---
+++ src/SampleApp.Domain/Entities/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using SampleApp.Domain.Common;
+using SampleApp.Domain.Enums;
+using System;
+
+namespace SampleApp.Domain.Entities
+{
+    public class TodoItem : AuditableEntity
+    {
+        public long Id { get; set; }
+
+        public int ListId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Note { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime? Reminder { get; set; }
+
+        public PriorityLevel Priority { get; set; }
+
+
+        public TodoList List { get; set; }
+    }
+}
