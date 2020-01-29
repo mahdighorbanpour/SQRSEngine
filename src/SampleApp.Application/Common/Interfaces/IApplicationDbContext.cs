a19c25c965cdff3189c33c71d3700a9ec62a53ed
Mahdi Ghorbanpoor <mahdi.ghorbanpoor@pgk.de>
2020-01-29 14:30:10 +0100

    Added Application layer and Infrastructure layer based on the CleanArchitechture sample

---
+++ src/SampleApp.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using SampleApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleApp.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<TodoList> TodoLists { get; set; }
+
+        DbSet<TodoItem> TodoItems { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/SampleApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using SampleApp.Application.Common.Interfaces;
+using SampleApp.Domain.Common;
+using SampleApp.Domain.Entities;
+using SampleApp.Infrastructure.Identity;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace SampleApp.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<TodoList> TodoLists { get; set; }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = System.DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = System.DateTime.Now;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
